c809aaa1ca4075facc972ba60892ff354c86ef52
TomiokaGiyuuu <islam.nugman.04@mail.ru>
2023-04-27 17:37:41 +0600

    chat page change

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -15,38 +15,103 @@ class _ChatState extends State<Chat> {
     return Scaffold(
         backgroundColor: const  Color(0xff282B30),
         body:
-        Container(
-          margin: const EdgeInsets.all(10.0),
-            color: const Color.fromARGB(255, 52, 56, 63),
-            child: SizedBox(
-              child: Row(
-                children:[
-                  Container(
-                  width: 100,
-                  height: 100,
-                  child: Padding(
-                      padding: const EdgeInsets.all(1.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(25.0),
-                        child: Image.asset('lib/images/profileImg.png'),
+            Column(
+              children: [
+                Container(
+                    margin: const EdgeInsets.all(5.0),
+                    color: const Color.fromARGB(255, 52, 56, 63),
+                    child: SizedBox(
+                      child: Row(
+                        children:[
+                          Container(
+                              width: 100,
+                              height: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  child: Image.asset('lib/images/nuka_chan.png'),
+                                ),
+                              )
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Text(
+                              'Nuka Chan',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.notoSerif(
+                                  color: Colors.white,
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
                       ),
-                  )
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(1.0),
-                    child: Text(
-                      'Tomioka',
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.notoSerif(
-                          color: Colors.white,
-                          fontSize: 24, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              ),
+                    )
+                ),
+                Container(
+                    margin: const EdgeInsets.all(5.0),
+                    color: const Color.fromARGB(255, 52, 56, 63),
+                    child: SizedBox(
+                      child: Row(
+                        children:[
+                          Container(
+                              width: 100,
+                              height: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  child: Image.asset('lib/images/islam_sensei.png'),
+                                ),
+                              )
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(1.0),
+                            child: Text(
+                              'Islam Sensei',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.notoSerif(
+                                  color: Colors.white,
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                ),
+                Container(
+                    margin: const EdgeInsets.all(5.0),
+                    color: const Color.fromARGB(255, 52, 56, 63),
+                    child: SizedBox(
+                      child: Row(
+                        children:[
+                          Container(
+                              width: 100,
+                              height: 100,
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  child: Image.asset('lib/images/arman_kun.png'),
+                                ),
+                              )
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(1.0),
+                            child: Text(
+                              'Arman Kun',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.notoSerif(
+                                  color: Colors.white,
+                                  fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                ),
+              ],
             )
-        ),
-
     );
   }
 }
